14aa896714b319e827e0e30549e403b910e809e9
gtosilindt <gtosi@lindt.com>
2026-06-17 20:14:01 +0200

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a237e',
+        'primary-light': '#3949ab',
+        'primary-dark': '#0d1642',
+      },
+    },
+  },
+  plugins: [],
+}
