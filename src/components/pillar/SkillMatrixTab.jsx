a62b2af88c0cdc8d3d609490d13f82e39528bf2f
giotosi94 <gio_tosi@hotmail.it>
2026-07-03 15:36:41 +0200

    Update SkillMatrixTab.jsx

--- src/components/pillar/SkillMatrixTab.jsx
+++ src/components/pillar/SkillMatrixTab.jsx
@@ -108,7 +108,6 @@ export default function SkillMatrixTab({ pillar, color }) {
       },
     }
     setMatrix({ ...matrix, valori: newValori })
-
     // Debounce salvataggio: salviamo dopo 500ms senza modifiche
     if (window._skillMatrixSaveTimer) clearTimeout(window._skillMatrixSaveTimer)
     window._skillMatrixSaveTimer = setTimeout(() => {
