3745d3e48935b35fd727cd1a8f3cfe5a992cd12d
giotosi94 <gio_tosi@hotmail.it>
2026-08-25 19:29:02 +0200

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -103,7 +103,7 @@ export default function Layout() {
         </div>
       </aside>
 
-<main className="flex-1 min-w-0 max-w-full overflow-x-hidden overflow-y-auto p-6">
+<main className="flex-1 overflow-y-auto p-6">
   <Outlet />
 </main>
     </div>
