6b4b77b0f827747d393397d2eda9a29b6cd3159b
giotosi94 <gio_tosi@hotmail.it>
2026-09-08 19:21:10 +0200

    Add ScrapDefineStep to COMPONENT_REGISTRY

--- src/components/major/componentRegistry.js
+++ src/components/major/componentRegistry.js
@@ -1,6 +1,9 @@
 import GenericStepData from './GenericStepData'
+import { ScrapDefineStep } from './routes/processScrapReduction'
 
-const COMPONENT_REGISTRY = {}
+const COMPONENT_REGISTRY = {
+  scrap_baseline: ScrapDefineStep,
+}
 
 export function getStepComponent(componente) {
   return COMPONENT_REGISTRY[componente] || GenericStepData
