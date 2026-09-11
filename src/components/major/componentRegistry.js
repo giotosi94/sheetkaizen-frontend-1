ade5d332aff338e890c3560cb8f56510fab0a978
giotosi94 <gio_tosi@hotmail.it>
2026-09-11 16:29:50 +0200

    Add AnomalyAnalysis to component registry

--- src/components/major/componentRegistry.js
+++ src/components/major/componentRegistry.js
@@ -2,11 +2,13 @@ import GenericStepData from './GenericStepData'
 import ScrapBaseline from './routes/processScrapReduction/ScrapBaseline'
 import BasicConditions from './routes/processScrapReduction/BasicConditions'
 import ScrapEventRegister from './routes/processScrapReduction/ScrapEventRegister'
+import AnomalyAnalysis from './routes/processScrapReduction/AnomalyAnalysis'
 
 const COMPONENT_REGISTRY = {
   scrap_baseline: ScrapBaseline,
   basic_conditions: BasicConditions,
   scrap_event_register: ScrapEventRegister,
+  anomaly_analysis: AnomalyAnalysis,
 }
 
 export function getStepComponent(componente) {
