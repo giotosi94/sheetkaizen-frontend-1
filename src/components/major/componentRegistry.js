ad155a152c8a44a91266696ccddf8493375d11e6
giotosi94 <gio_tosi@hotmail.it>
2026-09-11 16:54:13 +0200

    Update componentRegistry.js

--- src/components/major/componentRegistry.js
+++ src/components/major/componentRegistry.js
@@ -3,12 +3,14 @@ import ScrapBaseline from './routes/processScrapReduction/ScrapBaseline'
 import BasicConditions from './routes/processScrapReduction/BasicConditions'
 import ScrapEventRegister from './routes/processScrapReduction/ScrapEventRegister'
 import AnomalyAnalysis from './routes/processScrapReduction/AnomalyAnalysis'
+import EcrsAnalysis from './routes/processScrapReduction/EcrsAnalysis'
 
 const COMPONENT_REGISTRY = {
   scrap_baseline: ScrapBaseline,
   basic_conditions: BasicConditions,
   scrap_event_register: ScrapEventRegister,
   anomaly_analysis: AnomalyAnalysis,
+  ecrs_analysis: EcrsAnalysis,
 }
 
 export function getStepComponent(componente) {
