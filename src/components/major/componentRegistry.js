225636fc6f35b4875c287f89864de60085a4b361
giotosi94 <gio_tosi@hotmail.it>
2026-09-11 16:00:21 +0200

    Update componentRegistry.js

--- src/components/major/componentRegistry.js
+++ src/components/major/componentRegistry.js
@@ -1,8 +1,10 @@
 import GenericStepData from './GenericStepData'
 import ScrapBaseline from './routes/processScrapReduction/ScrapBaseline'
+import BasicConditions from './routes/processScrapReduction/BasicConditions'
 
 const COMPONENT_REGISTRY = {
   scrap_baseline: ScrapBaseline,
+  basic_conditions: BasicConditions,
 }
 
 export function getStepComponent(componente) {
