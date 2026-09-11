84e59355eb53542702f0b002632c7cf59f0d9088
giotosi94 <gio_tosi@hotmail.it>
2026-09-11 16:14:24 +0200

    Add ScrapEventRegister to component registry

--- src/components/major/componentRegistry.js
+++ src/components/major/componentRegistry.js
@@ -1,10 +1,12 @@
 import GenericStepData from './GenericStepData'
 import ScrapBaseline from './routes/processScrapReduction/ScrapBaseline'
 import BasicConditions from './routes/processScrapReduction/BasicConditions'
+import ScrapEventRegister from './routes/processScrapReduction/ScrapEventRegister'
 
 const COMPONENT_REGISTRY = {
   scrap_baseline: ScrapBaseline,
   basic_conditions: BasicConditions,
+  scrap_event_register: ScrapEventRegister,
 }
 
 export function getStepComponent(componente) {
