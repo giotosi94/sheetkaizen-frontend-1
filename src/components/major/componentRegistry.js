4b56e0aae785469de8cc267972b76f507cec13c9
giotosi94 <gio_tosi@hotmail.it>
2026-09-11 15:40:52 +0200

    Add ScrapBaseline to component registry

--- src/components/major/componentRegistry.js
+++ src/components/major/componentRegistry.js
@@ -1,6 +1,9 @@
 import GenericStepData from './GenericStepData'
+import ScrapBaseline from './routes/processScrapReduction/ScrapBaseline'
 
-const COMPONENT_REGISTRY = {}
+const COMPONENT_REGISTRY = {
+  scrap_baseline: ScrapBaseline,
+}
 
 export function getStepComponent(componente) {
   return COMPONENT_REGISTRY[componente] || GenericStepData
